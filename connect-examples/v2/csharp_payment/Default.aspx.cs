92676d77805da8e01409f4b4efd32a24e4e7ba30
Ti-Fen Pan <tpan@squareup.com>
2016-07-28 22:44:15 -0700

    init csharp example

---
+++ connect-examples/v2/csharp_payment/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Square.Connect.Api;
+using Square.Connect.Model;
+
+namespace SquareEcommerce
+{
+   
+    public partial class Default : System.Web.UI.Page
+    {
+        private static TransactionApi _transactionApi;
+        private static string _accessToken = "sq0atb-44gzjrtZTseuJK5BQqCfbg";
+        private static string _locationId = "CBASEFU1QAw5oZ261gN6WWyCXfA";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _transactionApi = new TransactionApi();
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string GetCardNonce(string nonce)
+        {
+            return "Hello " + nonce;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string Charge(string nonce)
+        {
+            string uuid = NewIdempotencyKey();
+            Money amount = NewMoney(100, "USD");
+            ChargeRequest body = new ChargeRequest(AmountMoney: amount, IdempotencyKey: uuid, CardNonce: nonce);
+            var response = _transactionApi.Charge(_accessToken, _locationId, body);
+            if (response.Errors == null)
+            {
+                return "Transaction complete\n" + response.ToJson();
+            } else
+            {
+                return response.Errors.ToString();
+            }
+        }
+
+        public static Money NewMoney(int amount, string currency)
+        {
+            return new Money(amount, Money.ToCurrencyEnum(currency));
+        }
+
+        public static string NewIdempotencyKey()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+
+}
